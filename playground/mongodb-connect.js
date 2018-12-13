37f65888f46016d52068516e8c2ef4d65aee2d9f
TomVanAken <tom.vanaken@groupwave.be>
2018-12-13 20:45:43 +0100

    Insert records without destructuring

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,46 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+//Object destucturing example
+var user = {name: 'Tom', age: 45};
+var {name} = user;
+console.log(name);
+//returns name from user.
+
+
+MongoClient.connect( 'mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        console.log("Unable to connect tot MongoDb Server");
+        console.log(err);
+        return "";
+    }
+
+    console.log('Connected to MongoDb');
+    const db = client.db('TodoApp');
+
+
+   /*  db.collection('Todos').insertOne({
+        text: 'Someting to do',
+        completed: false
+    }, (err, result) => {
+        if(err) {
+            console.log('Unable to insert Todo', err);
+            return;
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    }) */
+
+    db.collection('Users').insertOne({
+        name: "Tom",
+        age: 45,
+        location: "Ekeren"
+    }, (err, result) => {
+        if(err) {
+            return console.log("Unable to create user", err);
+        }
+        
+        console.log(result.ops[0]._id.getTimestamp());
+    })
+
+    client.close();
+})
