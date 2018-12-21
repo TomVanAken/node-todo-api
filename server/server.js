9241c192bbe3b8f0d70d43d4ce3be8ee90235a5e
TomVanAken <tom.vanaken@groupwave.be>
2018-12-21 11:18:16 +0100

    Refactored and first Post request call

--- server/server.js
+++ server/server.js
@@ -1,71 +1,31 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 
+const app = express();
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String, 
-        required: true,
-        minlength: 1,
-        trim: true
-    }, 
-    completed: {
-        type: Boolean,
-        default: false
-    }, 
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
+app.use(bodyParser.json());
 
-// var newTodo = new Todo({
-//     text: "Cook diner"
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log("Saved Todo", doc);
-// }, (e) => {
-//     //Error handling save
-//     console.log("Unable to save Todo");
-// })
-
-//Challenge
-// var nextTodo = new Todo({
-//     text: "   Have a meeting   ",
+//app.post(url, callback)
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
     
-// });
-
-// nextTodo.save().then((doc) => {
-//     console.log("Todo saved");
-//     console.log(doc);
-// }, (e) => {
-//     console.log("Unable to save", e);
-// })
-
-
-
-var User = mongoose.model("User", {
-        email: {
-            type: String,
-            required: true,
-            minlength: 1,
-            trim: true
-        }
-});
-
-var newUser= new User({
-    email: ' tom@test.com   '
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        //Set status 400 to 
+        res.status(400).send(e);
+    })
 })
 
-newUser.save().then((doc) => {
-    console.log("New User saved");
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log('Unable to save new User');
-    console.log("Message: ", e.message);
-    console.log("Errors: ", e.errors);
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 })
