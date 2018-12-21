cc574852fa6d058ac8aba0a1d3deff8a3f2e8692
TomVanAken <tom.vanaken@groupwave.be>
2018-12-21 18:52:59 +0100

    added GET /todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos,
+            count: todos.length})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
 
 
 app.listen(3000, () => {
